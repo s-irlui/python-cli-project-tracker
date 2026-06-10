59cd24934cb19d3a8d250069c6ba4139795ccc5b
Louis Gitonga <louisgitonga19@gmail.com>
2026-06-10 10:13:34 +0300

    Add user project and task models

---
+++ app/models.py
@@ -0,0 +1,49 @@
+class User:
+    def __init__(self, user_id, name, email):
+        self.user_id = user_id
+        self.name = name
+        self.email = email
+
+    def to_dict(self):
+        return {
+            "user_id": self.user_id,
+            "name": self.name,
+            "email": self.email
+        }
+
+
+class Project:
+    def __init__(self, project_id, title, description, user_id):
+        self.project_id = project_id
+        self.title = title
+        self.description = description
+        self.user_id = user_id
+
+    def to_dict(self):
+        return {
+            "project_id": self.project_id,
+            "title": self.title,
+            "description": self.description,
+            "user_id": self.user_id
+        }
+
+
+class Task:
+    def __init__(self, task_id, title, project_id, contributors=None, completed=False):
+        self.task_id = task_id
+        self.title = title
+        self.project_id = project_id
+        self.contributors = contributors if contributors else []
+        self.completed = completed
+
+    def mark_complete(self):
+        self.completed = True
+
+    def to_dict(self):
+        return {
+            "task_id": self.task_id,
+            "title": self.title,
+            "project_id": self.project_id,
+            "contributors": self.contributors,
+            "completed": self.completed
+        }
